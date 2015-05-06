2d42e0b85165aa557380be0801ddc6b62297b5ba
amd989 <mail@alejandro.md>
2015-05-05 22:02:55 -0600

    Adding Assembly information version

--- Symlink Creator/Properties/AssemblyInfo.cs
+++ Symlink Creator/Properties/AssemblyInfo.cs
@@ -43,4 +43,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.2.0")]
 [assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyInformationalVersion("1.1.2.0")]
 [assembly: NeutralResourcesLanguage("en")]

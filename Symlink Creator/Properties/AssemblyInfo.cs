8a8a032d817503e6ff6702e4cd54e67ac7052422
Alejandro Mora <mail@alejandro.md>
2016-09-30 18:54:08 -0400

    Getting versions synced

--- Symlink Creator/Properties/AssemblyInfo.cs
+++ Symlink Creator/Properties/AssemblyInfo.cs
@@ -41,7 +41,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2.2")]
-[assembly: AssemblyFileVersion("1.1.2.2")]
+[assembly: AssemblyVersion("1.1.2.15")]
+[assembly: AssemblyFileVersion("1.1.2.15")]
 [assembly: AssemblyInformationalVersion("1.1.2.0")]
 [assembly: NeutralResourcesLanguage("en")]

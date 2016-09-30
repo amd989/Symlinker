ab959754a309875418f70f152a680b01d73a4db4
Alejandro Mora <mail@alejandro.md>
2016-09-30 18:36:19 -0400

    Adding valid certificate
    Trying to sign with a valid certificate.

--- Symlink Creator/Properties/AssemblyInfo.cs
+++ Symlink Creator/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alejandro Mora")]
 [assembly: AssemblyProduct("Symlink Creator")]
-[assembly: AssemblyCopyright("Copyright © Alejandro Mora 2015")]
+[assembly: AssemblyCopyright("Copyright © Alejandro Mora 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -41,7 +41,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.2.2")]
+[assembly: AssemblyFileVersion("1.1.2.2")]
 [assembly: AssemblyInformationalVersion("1.1.2.0")]
 [assembly: NeutralResourcesLanguage("en")]
